a77de99e109a64440c76d3d4f055db376cee966a
Elias Ikävalko <elias.ikavalko99@gmail.com>
2020-01-21 22:15:52 +0200

    testit ja circleci

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -39,7 +39,7 @@ public class VarastoTest {
     }
 
     @Test
-    public void lisaysLisaaPienentaaVapaataTilaa() {
+    public void lisaysPienentaaVapaataTilaa() {
         varasto.lisaaVarastoon(8);
 
         // vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
@@ -64,5 +64,59 @@ public class VarastoTest {
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
-
+    
+    @Test
+    public void eiVoiLisataLiikaaTavaraa() {
+        varasto.lisaaVarastoon(20);
+        
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void eiVoiOttaaLiikaaTavaraa() {
+        varasto.lisaaVarastoon(10);
+        varasto.otaVarastosta(20);
+        
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void negatiivinenMaaraEiLisaaVarastoon() {
+        varasto.lisaaVarastoon(-10);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void negatiivinenMaaraEiOtaVarastosta() {
+        varasto.otaVarastosta(-10);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void virheellinenTilavuusLuoKayttokelvottomanVaraston() {
+        varasto = new Varasto(-1);
+        assertEquals(0, varasto.getTilavuus(), vertailuTarkkuus);
+        varasto = new Varasto(-1, 10);
+        assertEquals(0, varasto.getTilavuus(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void varastonVoiLuodaAlkusaldolla() {
+        varasto = new Varasto(10, 10);
+        
+        assertEquals(10, varasto.getTilavuus(), vertailuTarkkuus);
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void varastoaEiVoiLuodaNegatiivisellaAlkusaldolla() {
+        varasto = new Varasto(10, -10);
+        
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void varastonMerkkijonoesitysOnOikein() {
+        assertEquals("saldo = 0.0, vielä tilaa 10.0", varasto.toString());
+    }
 }
